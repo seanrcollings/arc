bef0b3ccf35e31b020931793c5fa9947116b0490
Sean Collings <seanrcollings@gmail.com>
2021-05-24 22:48:59 -0600

    version bump

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="arc-cli",
-    version="2.2.1",
+    version="2.3.0",
     license="MIT",
     author="Sean Collings",
     author_email="sean@seanrcollings.com",
     description="A Regular CLI",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    download_url="https://github.com/seanrcollings/arc/archive/v2.2.1tar.gz",
+    download_url="https://github.com/seanrcollings/arc/archive/v2.3.0tar.gz",
     url="https://github.com/seanrcollings/arc",
     keywords=["CLI", "extendable", "easy"],
     packages=setuptools.find_packages("src"),
