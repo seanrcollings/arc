0b600269e53e52a269fe68bccb56c6d942dbe988
Sean Collings <seanrcollings@gmail.com>
2021-07-10 22:41:50 -0600

    completion cleanup

--- arc/autocomplete/fish.py
+++ arc/autocomplete/fish.py
@@ -18,8 +18,15 @@ class FishCompletion:
         self.flush()
         return "\n".join(self.lines)
 
-    def add(self, content: str):
-        self.lines.append(f"{COMPLETE}{self.command}{content}")
+    def add(self, content: str, no_format=False):
+        if no_format:
+            self.lines.append(content)
+        else:
+            self.lines.append(f"{COMPLETE}{self.command}{content}")
+
+    def comment(self, comment: str):
+        self.add(f"\n# {comment}", no_format=True)
+        return self
 
     def flush(self):
         if self._buffer:
@@ -30,53 +37,44 @@ class FishCompletion:
 
     def _buffer_add(self, content: str):
         self._buffer += " " + content
+        return self
 
     def set(self, name: str, value: str):
         self.lines.append(f"set -l {name} {value}")
         return self
 
     def no_files(self):
-        self._buffer_add("-f")
-        return self
+        return self._buffer_add("-f")
 
     def force_files(self):
-        self._buffer_add("-F")
-        return self
+        return self._buffer_add("-F")
 
     def description(self, desc: str):
-        self._buffer_add(f'-d "{desc}"')
-        return self
+        return self._buffer_add(f'-d "{desc}"')
 
     def arguments(self, args: Union[list[str], str]):
         if isinstance(args, list):
             args = " ".join(args)
 
-        self._buffer_add(f'-a "{args}"')
-        return self
+        return self._buffer_add(f'-a "{args}"')
 
     def long(self, name: str):
-        self._buffer_add(f"-l {name}")
-        return self
+        return self._buffer_add(f"-l {name}")
 
     def short(self, name: str):
-        self._buffer_add(f"-s {name}")
-        return self
+        return self._buffer_add(f"-s {name}")
 
     def keep_order(self):
-        self._buffer_add("-k")
-        return self
+        return self._buffer_add("-k")
 
     def required(self):
-        self._buffer_add("-r")
-        return self
+        return self._buffer_add("-r")
 
     def exclusive(self):
-        self._buffer_add("-x")
-        return self
+        return self._buffer_add("-x")
 
     def condition(self, condition: str):
-        self._buffer_add(f'-n "{condition}"')
-        return self
+        return self._buffer_add(f'-n "{condition}"')
 
     def seen_subcommand(self, *args: str):
         return self.condition(f"{SEEN_SUBCOMMAND} {' '.join(args)}")
@@ -86,20 +84,29 @@ class FishCompletion:
 
 
 def generate(name: str, root: Command):
-    # comp = f"complete -c {name}"
-    commands = helpers.get_all_commands(root)
+    commands = [
+        command for command in helpers.get_all_commands(root) if command[0] != root
+    ]
 
     completions = (
         FishCompletion(name)
+        .comment("Setup")
         .set("commands", " ".join(name for _, name in commands))
         .no_files()
         .flush()
     )
 
-    for command, cmd_name in commands:
-        if command == root:
-            continue
+    completions.comment("Commands")
+    command_completions(completions, commands)
+    argument_completions(completions, commands)
 
+    print(completions)
+
+
+def command_completions(
+    completions: FishCompletion, commands: list[tuple[Command, str]]
+):
+    for command, cmd_name in commands:
         desc = command.doc.split("\n")[0]
         (
             completions.not_seen_subcommand("$commands")
@@ -108,15 +115,25 @@ def generate(name: str, root: Command):
             .flush()
         )
 
+
+def argument_completions(
+    completions: FishCompletion, commands: list[tuple[Command, str]]
+):
     for command, cmd_name in commands:
-        if command == root:
+        parse_type = type(command.parser)
+
+        if not command.parser.args or parse_type in (
+            ParsingMethod.POSITIONAL,
+            ParsingMethod.RAW,
+        ):
             continue
 
+        completions.comment(f"{cmd_name} Arguments")
         for arg in command.parser.args.values():
+
             if arg.hidden:
                 continue
 
-            parse_type = type(command.parser)
             if arg.is_flag():
                 (
                     completions.seen_subcommand(cmd_name)
@@ -125,7 +142,7 @@ def generate(name: str, root: Command):
                 )
 
             elif parse_type is ParsingMethod.STANDARD:
-                (completions.seen_subcommand(cmd_name).required().long(arg.name))
+                completions.seen_subcommand(cmd_name).required().long(arg.name)
             elif parse_type is ParsingMethod.KEYWORD:
                 completions.seen_subcommand(cmd_name).arguments(
                     f"{arg.name}{config.arg_assignment}"
@@ -137,5 +154,3 @@ def generate(name: str, root: Command):
                 completions.no_files()
 
             completions.flush()
-
-    print(completions)
