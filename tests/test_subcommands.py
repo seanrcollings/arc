2d8af8ca8a1a5734bb4ea2145a91da12f12724a4
Sean Collings <seanrcollings@gmail.com>
2023-02-24 22:16:28 -0700

    added test for different command creation convetions

--- tests/test_subcommands.py
+++ tests/test_subcommands.py
@@ -28,3 +28,29 @@ def test_aliases():
     assert ns("s") == "sub"
     assert ns("other-sub") == "other_sub"
     assert ns("os") == "other_sub"
+
+
+def test_creation_conventions():
+    ns = arc.namespace("ns")
+
+    @ns.subcommand
+    def sub1():
+        return True
+
+    def sub2():
+        return True
+
+    ns.subcommand(sub2)
+
+    @ns.subcommand()
+    def sub3():
+        return True
+
+    @ns.subcommand("changed-name")
+    def sub4():
+        return True
+
+    assert ns("sub1")
+    assert ns("sub2")
+    assert ns("sub3")
+    assert ns("changed-name")
