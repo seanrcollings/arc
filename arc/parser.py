2362f1c6ceb788e25759eca15178017fbf23632c
Sean Collings <me@seancollings.dev>
2023-12-11 17:10:08 -0700

    --help and --version will write to stdout instead of stderr

--- arc/parser.py
+++ arc/parser.py
@@ -84,7 +84,6 @@ class Parser(argparse.ArgumentParser):
         arg_string_pattern_parts = []
         arg_strings_iter = iter(arg_strings)
         for i, arg_string in enumerate(arg_strings_iter):
-
             # all args after -- are non-options
             if arg_string == "--":
                 arg_string_pattern_parts.append("-")
@@ -131,7 +130,6 @@ class Parser(argparse.ArgumentParser):
 
         # function to convert arg_strings into an optional action
         def consume_optional(start_index):  # type: ignore
-
             # get the optional identified at this index
             option_tuple = option_string_indices[start_index]
             action, option_string, explicit_arg = option_tuple
@@ -156,7 +154,6 @@ class Parser(argparse.ArgumentParser):
             # ---------------------------------------------------
             action_tuples = []
             while True:
-
                 # if we found no optional action, skip it
                 if action is None:
                     extras.append(arg_strings[start_index])
@@ -249,7 +246,6 @@ class Parser(argparse.ArgumentParser):
         else:
             max_option_string_index = -1
         while start_index <= max_option_string_index:
-
             # consume any Positionals preceding the next option
             next_option_string_index = min(
                 [index for index in option_string_indices if index >= start_index]
@@ -353,13 +349,13 @@ class CustomAction(argparse.Action):
 
 class CustomHelpAction(CustomAction, argparse._HelpAction):
     def __call__(self, *args: t.Any, **kwargs: t.Any) -> None:
-        arc.info(self.command.doc.help())
+        arc.log(self.command.doc.help())
         arc.exit()
 
 
 class CustomVersionAction(CustomAction, argparse._VersionAction):
     def __call__(self, *args: t.Any, **kwargs: t.Any) -> None:
-        arc.info(self.command.config.version)
+        arc.log(self.command.config.version)
         arc.exit()
 
 
