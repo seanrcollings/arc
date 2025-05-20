e63c082f1a0e3658bae34aa9b6cef2514f6edfb1
Sean Collings <sean.collings@atomicjolt.com>
2025-05-20 15:28:44 -0600

    fix: Pylance symbols fix

--- arc/__init__.py
+++ arc/__init__.py
@@ -26,3 +26,54 @@ from arc.runtime import App, ExecMiddleware, InitMiddleware, Context
 from arc.types import State, convert
 from arc.autocompletions import Completion, CompletionInfo
 from arc.version import __version__
+
+__all__ = [
+    # Config
+    "ColorConfig",
+    "Config",
+    "LinksConfig",
+    "SuggestionConfig",
+    "PresentConfig",
+    "PluginConfig",
+    "configure",
+    # Define
+    "Command",
+    "Param",
+    "Argument",
+    "Count",
+    "Depends",
+    "Flag",
+    "Option",
+    "command",
+    "group",
+    "namespace",
+    # Errors
+    "ConversionError",
+    "ExecutionError",
+    "ValidationError",
+    "exit",
+    # Present
+    "err",
+    "info",
+    "pager",
+    "print",
+    "usage",
+    "log",
+    "markdown",
+    "parse_markdown",
+    # Prompt
+    "Prompt",
+    # Runtime
+    "App",
+    "ExecMiddleware",
+    "InitMiddleware",
+    "Context",
+    # Types
+    "State",
+    "convert",
+    # Autocompletions
+    "Completion",
+    "CompletionInfo",
+    # Version
+    "__version__",
+]
